6e4b22f5332e608d420ec166e917d3b1a5e1a311
talhahamid29 <talha.syed@oxcytech.com>
2024-01-18 12:22:43 +0530

    'website'

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -20,7 +20,7 @@ const Footer: React.FC = () => {
                 d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
               />
             </svg>
-            University
+            <p className="mt-[10px]">University</p>
           </p>
           <p className="mt-10 text-gray-400 text-sm p-2 ">9876543210</p>
           <p className="text-sm p-2 text-gray-400">
